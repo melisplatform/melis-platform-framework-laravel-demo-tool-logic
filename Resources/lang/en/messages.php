a4d6a99288f3cd116936fec30d68fe0223db653a
bryan <rbbrioso28@gmail.com>
2019-09-03 19:32:42 +0800

    Translations updated

--- Resources/lang/en/messages.php
+++ Resources/lang/en/messages.php
@@ -2,14 +2,14 @@
 
 return [
     'album' => 'Album',
-    'album_message' => 'The table below has been created from a simple query through the Laravel Query Builder located in <span class="text-danger">Http/Controllers/IndexController.php</span>. <br>It is using the database of Melis Platform.',
+    'album_message' => 'The table below shows the data fetched from the database using Laravel.',
 	'id' => 'ID',
 	'album_name' => 'Album name',
 	'songs' => 'Songs',
 	'date' => 'Date',
 
 	'core_lang' => 'Melis Core Languages',
-	'core_lang_message' => 'The table below has been created using Zend Service Manager. One of the services provided allows a database connection to fetch data.',
+	'core_lang_message' => 'The table below shows the data fetched from Melis Platform Services using Laravel.',
 	'locale' => 'Locale',
 	'name' => 'Name',
 ];
--- Resources/lang/fr/messages.php
+++ Resources/lang/fr/messages.php
@@ -2,14 +2,14 @@
 
 return [
     'album' => 'Album',
-    'album_message' => 'Le tableau ci-dessous a été créé à partir d\'une simple requête via le Query Builder de Laravel situé dans <span class="text-danger">Http/Controllers/IndexController.php</span>. <br>Il utilise la base de données de Melis Platform.',
+    'album_message' => 'Le tableau ci-dessous affiche les données extraites de la base de données en utilisant Laravel.',
 	'id' => 'ID',
 	'album_name' => 'Nom de l\'album',
 	'songs' => 'Chansons',
 	'date' => 'Date',
 
 	'core_lang' => 'Langages Melis Core',
-	'core_lang_message' => 'Le tableau ci-dessous a été créé en utilisant Zend Service Manager. L\'un des services fournis permet une connection à la base de données pour récupérer des données.',
+	'core_lang_message' => 'Le tableau ci-dessous affiche les données extraites des services de Melis Platform en utilisant Laravel.',
 	'locale' => 'Locale',
 	'name' => 'Nom',
 ];
