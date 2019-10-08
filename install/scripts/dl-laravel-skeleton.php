2e383c490209a9622d1a8c9ad947b0c58f059090
Cris <crismakabenta@gmail.com>
2019-10-08 13:07:33 +0800

    Added return value on script

--- install/scripts/dl-laravel-skeleton.php
+++ install/scripts/dl-laravel-skeleton.php
@@ -5,5 +5,11 @@ $isCliReqs = php_sapi_name() == 'cli' ? true : false;
 //third party file
 $thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Laravel' : 'thirdparty/Laravel';
 
-if (!is_dir($thirdPartyFolder))
-    MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('laravel');
+if (!is_dir($thirdPartyFolder)) {
+    return MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('laravel');
+}else{
+    return [
+        'success' => true,
+        'message' => 'Laravel skeleton downloaded successfully'
+    ];
+}
