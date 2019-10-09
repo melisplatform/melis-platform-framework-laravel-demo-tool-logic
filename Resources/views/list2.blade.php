9ddf92280428d8ed29b9fe82ec4cb05fa264f864
bryan <rbbrioso28@gmail.com>
2019-10-09 12:00:33 +0800

    View localization set to en

--- Resources/views/list2.blade.php
+++ Resources/views/list2.blade.php
@@ -1,13 +1,13 @@
 <div class="innerAll spacing-x2">
-    <h2>{{ __('laravelDemoTool::messages.album') }}</h2>
-    <p>{!! __('laravelDemoTool::messages.album_message') !!}</p>
+    <h2>Album</h2>
+    <p>The table below shows the data fetched from the database using Laravel.</p>
     <pre>$albums = DB::table('melis_demo_album')->get();</pre>
     <table class="table">
         <tr style=" background: #ff2d20; color: #fff;">
-            <th>{{ __('laravelDemoTool::messages.id') }}</th>
-            <th>{{ __('laravelDemoTool::messages.album_name') }}</th>
-            <th>{{ __('laravelDemoTool::messages.songs') }}</th>
-            <th>{{ __('laravelDemoTool::messages.date') }}</th>
+            <th>ID</th>
+            <th>Album name</th>
+            <th>Songs</th>
+            <th>Date</th>
         </tr>
         @foreach($albums As $album)
             <tr>
